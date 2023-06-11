fbb4c7e857007f90cd6ed82f7873a0c52c30c0ce
Alper Can Bulut <abulut1995@hotmail.com>
2023-06-11 19:29:58 +0300

    structure

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoForm from './TodoForm'
+
+const TodoWrapper = () => {
+  return (
+
+    <div className='TodoWrapper'>
+        <TodoForm/>
+       
+    </div>
+  )
+}
+
+export default TodoWrapper
