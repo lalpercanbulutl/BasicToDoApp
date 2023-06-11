e22fedead02117875de4198174b730b4eb0cb6d5
Alper Can Bulut <abulut1995@hotmail.com>
2023-06-11 20:32:34 +0300

    prop,useState,form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({addTodo}) => { //prop olarak addTodo yazdık 
+    const [value,setValue] = useState("")
+    const handleChange = (e) => {
+       setValue(e.target.value)
+      };
+    const handleSubmit = (e) => {
+        console.log("e: ",value)
+        e.preventDefault(); // prevent default action
+       addTodo(value) //value'yü addTodo propuna gönderdik bunuda TodoWrapperında kullanabileceğiz
+       setValue('') //after submit to delete writings
+    };
+    
+
   return (
-    <form className='TodoForm'>
-    <input type="text" className='todo-input' placeholder='What is the task today?'/>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+    <input type="text" className='todo-input' placeholder='What is the task today?' 
+    value= {value}
+    onChange={handleChange}/>
     <button type='submit' className='todo-btn'>
         Add Task
     </button>
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import TodoForm from './TodoForm'
-
+import { v4 as uuidv4 } from "uuid";
+uuidv4(); //for initilazing
 const TodoWrapper = () => {
+    const [todos,setTodos] = useState([])
+    const addTodo = todo => {
+      
+        setTodos([
+            ...todos,
+            { id: uuidv4(), task: todo, completed: false, isEditing: false },
+          ]);
+        //...todos for get to current todos ,then ve add them id with uuidv4
+        console.log("value: ",todos)
+    }
   return (
 
     <div className='TodoWrapper'>
-        <TodoForm/>
+        <TodoForm addTodo={addTodo} />
        
     </div>
   )
